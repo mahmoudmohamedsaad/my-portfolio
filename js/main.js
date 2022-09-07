e47e3bf40a9833cdc7ea7d4332d661ac7d93f9a7
M7moud S3d <62154958+mahmoudmohamedsaad@users.noreply.github.com>
2022-09-07 09:32:51 -0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/*********************typing animation******************************/
+var typed = new Typed(".typing",{
+    strings: [" " ,"web designer", "Web Developer" , "UI/UX Designer","Freelancer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+});
